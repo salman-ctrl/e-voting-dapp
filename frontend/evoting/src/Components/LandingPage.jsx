b08a4a63dd59173b5ea270f92a64cf39a7b252cd
salman-ctrl <zirmanvictory@gmail.com>
2025-03-14 23:55:17 +0700

    add cards in landing page

--- frontend/evoting/src/Components/LandingPage.jsx
+++ frontend/evoting/src/Components/LandingPage.jsx
@@ -4,6 +4,10 @@ const LandingPage = () => {
     return (
         <div>
             <h1>Landing Page</h1>
+            <div className='w-72 card'>
+                <button>Click me</button>
+
+            </div>
         </div>
     )
 }
