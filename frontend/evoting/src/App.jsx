2e08fd3764aa1eadc106af956ff97efd04a1adf3
salman-ctrl <zirmanvictory@gmail.com>
2025-03-14 23:52:04 +0700

    tambah halaman landing page ke app

--- frontend/evoting/src/App.jsx
+++ frontend/evoting/src/App.jsx
@@ -3,12 +3,15 @@ import { ethers } from 'ethers';
 import { contractAbi, contractAddress } from './Constant/constan';
 import Login from './Components/Login';
 
+
 import React from 'react'
+import LandingPage from './Components/LandingPage';
 
 const App = () => {
   return (
     <div>
       <Login />
+      <LandingPage />
 
     </div>
   )
