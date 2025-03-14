5c8f750c967b5ff9ea09fe28b5658cdefd0c9d37
salman-ctrl <zirmanvictory@gmail.com>
2025-03-14 23:56:54 +0700

    add form in login

--- frontend/evoting/src/Components/Login.jsx
+++ frontend/evoting/src/Components/Login.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Login = () => {
     return (
         <div >
-            <Login />
+            <h1> Login </h1>
+            <form action="#">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required />
+                <br />
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     )
 }
