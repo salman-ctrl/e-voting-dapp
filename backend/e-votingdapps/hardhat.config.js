8825b02b75cefdeb62abf0ee0adc345d3ee6f30e
salman-ctrl <zirmanvictory@gmail.com>
2025-03-14 23:33:31 +0700

    first commit

---
+++ backend/e-votingdapps/hardhat.config.js
@@ -0,0 +1,31 @@
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.28",
+// };
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.28",
+  defaultNetwork: "volta",
+  networks: {
+    hardhat: {},
+    volta: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      gas: 210000000,
+      gasPrice: 800000000000,
+    }
+  },
+}
