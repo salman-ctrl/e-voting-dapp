40c118cd253e824c675e3fda0aab2f9ef75c1f83
salman-ctrl <zirmanvictory@gmail.com>
2025-03-16 02:08:06 +0700

    FIXING BUG MORE MORE MORE!

--- frontend/evoting/src/App.jsx
+++ frontend/evoting/src/App.jsx
@@ -86,95 +86,88 @@ const App = () => {
     }
   }
 
-  async function getCurrentStatus() {
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    const signer = await provider.getSigner();
-    const contractInstance = new ethers.Contract(
-      contractAddress, contractAbi, signer
-    );
 
-    const status = await contractInstance.getVotingStatus();
-    console.log(status);
-    setVotingStatus(status);
-  }
+  const status = await contractInstance.getVotingStatus();
+  console.log(status);
+  setVotingStatus(status);
+}
 
-  async function handleNumberChange(e) {
-    setNumber(e.target.value);
-  }
+async function handleNumberChange(e) {
+  setNumber(e.target.value);
+}
 
-  async function getRemainingTime() {
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    const signer = await provider.getSigner();
-    const contractInstance = new ethers.Contract(
-      contractAddress, contractAbi, signer
-    );
-    const time = await contractInstance.getRemainingTime();
-    setRemainingTime(parseInt(time, 16));
-  }
-
-  const handleAccountChange = (accounts) => {
-    if (accounts.length > 0 && account !== accounts[0]) {
-      setAccount(accounts[0]);
-      canVote();
-    } else {
-      setIsConnected(false);
-      setAccount(null);
-    }
-  }
-
-  async function connectToMetamask() {
-    if (window.ethereum) {
-      try {
-        // Untuk ethe rs v6
-        const provider = new ethers.BrowserProvider(window.ethereum);
-        setProvider(provider);
-
-        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
-        const signer = await provider.getSigner();
-        const address = await signer.getAddress();
-        setAccount(address);
-
-
-        Swal.fire({
-          title: "Metamask Anda sudah terhubung",
-          icon: "success",
-          draggable: true
-        });
-        console.log("Metamask Connected: " + address);
-        canVote();
-        setIsConnected(true);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    else {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Sepertinya Metamask tidak Terdeteksi di Browser ini",
-        footer: '<a href="#">Butuh Bantuan?</a>'
-      });
-      console.error("Metamask is not detected in the browser");
-    }
-  }
-
-  return (
-    <div className='w-screen h-screen justify-center flex-col flex items-center'>
-
-      {votingStatus ? (isConnected ? (<Connected
-        address={account}
-        candidate={candidates}
-        remainingTime={remainingTime}
-        number={number}
-        handleNumberChange={handleNumberChange}
-        voteFunction={vote}
-        showButton={canVote} />)
-
-        : (<Login connectWallet={connectToMetamask} />)) : <Finished />}
-    </div>
+async function getRemainingTime() {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  await window.ethereum.request({ method: "eth_requestAccounts" });
+  const signer = await provider.getSigner();
+  const contractInstance = new ethers.Contract(
+    contractAddress, contractAbi, signer
   );
+  const time = await contractInstance.getRemainingTime();
+  setRemainingTime(parseInt(time, 16));
+}
+
+const handleAccountChange = (accounts) => {
+  if (accounts.length > 0 && account !== accounts[0]) {
+    setAccount(accounts[0]);
+    canVote();
+  } else {
+    setIsConnected(false);
+    setAccount(null);
+  }
+}
+
+async function connectToMetamask() {
+  if (window.ethereum) {
+    try {
+      // Untuk ethe rs v6
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      setProvider(provider);
+
+      const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress();
+      setAccount(address);
+
+
+      Swal.fire({
+        title: "Metamask Anda sudah terhubung",
+        icon: "success",
+        draggable: true
+      });
+      console.log("Metamask Connected: " + address);
+      canVote();
+      setIsConnected(true);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Sepertinya Metamask tidak Terdeteksi di Browser ini",
+      footer: '<a href="#">Butuh Bantuan?</a>'
+    });
+    console.error("Metamask is not detected in the browser");
+  }
+}
+
+return (
+  <div className='w-screen h-screen justify-center flex-col flex items-center'>
+
+    {votingStatus ? (isConnected ? (<Connected
+      address={account}
+      candidate={candidates}
+      remainingTime={remainingTime}
+      number={number}
+      handleNumberChange={handleNumberChange}
+      voteFunction={vote}
+      showButton={canVote} />)
+
+      : (<Login connectWallet={connectToMetamask} />)) : <Finished />}
+  </div>
+);
 }
 
 export default App;
